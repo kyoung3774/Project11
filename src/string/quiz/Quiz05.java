837ea3f1355a59fc2178e10be0ebefdbae4a1989
BaeJooHwan <kyoung3774@naver.com>
2024-07-22 13:35:21 +0900

    2024.07.22

--- src/string/quiz/Quiz05.java
+++ src/string/quiz/Quiz05.java
@@ -16,16 +16,29 @@ public class Quiz05 {
 		System.out.print(str.charAt(3));
 		System.out.print(str.charAt(2));
 		System.out.print(str.charAt(1));
-		System.out.print(str.charAt(0));
-		
-		System.out.println();
+		System.out.println(str.charAt(0));
+
+		System.out.println("----1----");
 
 		// 2.문자열에서 뒤에서부터 한 글자씩 출력
 		int i = str.length() - 1;
 		while (i >= 0) {
 			System.out.print(str.charAt(i));
 			i--;
+			if (str.charAt(i) == str.charAt(0)) {
+				System.out.println(str.charAt(i));
+				i--;
+			}
 		}
+		System.out.println("----2----");
+
+		for (int j = str.length() - 1; j >= 0; j--) {
+			System.out.print(str.charAt(j));
+			if (str.charAt(j) == str.charAt(0)) {
+				
+			}
+		}
+
 	}
 
 }
